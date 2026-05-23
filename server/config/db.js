acab3b23616ad6295b4f98d8fada681012d92924
xronicus2005-cpu <xronicus2005@gmail.com>
2026-05-23 17:48:03 +0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -6,10 +6,11 @@ dotenv.config()
 const { Pool } = pg
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 })
 
 export default pool
